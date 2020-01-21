a199ea2a4b9bf623a75916fc813557b8cb0b8395
Eseniya <esenka564@gmail.com>
2020-01-22 00:13:07 +0300

    g

--- SimpleList/List.h
+++ SimpleList/List.h
@@ -80,26 +80,24 @@ public:
 
 		Node* prev = head;
 		current = head->getNext();
-		if (head != tail)
+		if (head = tail)
+			return;
+		while (current != tail)
 		{
-			while (current != tail)
+			if (min == current->getItem())
 			{
-				if (min == current->getItem())
-				{
-					Node* oldCurrent = current;
-					current = current->getNext();
-					delete oldCurrent;
-					prev->setNext(current);
-				}
-				else
-				{
-					prev = current;
-					current = current->getNext();
-				}
+				Node* oldCurrent = current;
+				current = current->getNext();
+				delete oldCurrent;
+				prev->setNext(current);
+			}
+			else
+			{
+				prev = current;
+				current = current->getNext();
 			}
 		}
-		else
-			return;
+		
 	}
 	
 	friend std::ostream& operator<<(std::ostream& out, List& list);
